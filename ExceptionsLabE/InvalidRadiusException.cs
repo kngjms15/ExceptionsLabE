8215d4fe71b8b5f18e7abc97daea44bea8ed86d8
kngjms15 <118232516+kngjms15@users.noreply.github.com>
2023-03-10 09:49:45 -0700

    Add project files.

---
+++ ExceptionsLabE/InvalidRadiusException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsLabE
+{
+    internal class InvalidRadiusException : Exception
+    {
+        //or overide the property
+        /**
+        public override string Message
+        {
+            get
+            {
+                return "Invalid radius";
+            }
+        }
+         **/
+        public double Radius { get; set; }
+        public InvalidRadiusException() : base("Invalid radius") // if without base.. will return generic exceptio or if base() will call null arg constructor
+        {
+
+        }
+        public InvalidRadiusException(double radius) : base($"Invalid radius: {radius}") // same as base ("Invalid radius: " + radius))
+        {
+            this.Radius = radius;
+        }
+    }
+}
---
+++ ExceptionsLabE/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionsLabE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double positiveRadius = 100;
+            double negativeRadius = -100;
+            double zeroRadius = 0;
+
+
+            try
+            {
+                Circle positiveCircle = new Circle(positiveRadius); // object 1 with positive radius
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+            try
+            {
+                Circle negativeCircle = new Circle(negativeRadius); // object 2 with negative radius
+            }
+            catch (InvalidRadiusException ex)
+            {
+                Console.WriteLine($"The radius " + ex.Radius + "is invalid" );
+                Console.WriteLine("Oh no! Something bad happened, but we can still go on. ");
+            }
+
+
+            try
+            {
+                Circle zeroCircle = new Circle(zeroRadius); // object 3 with zero
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Radius is 0");
+            } 
+
+            Console.ReadKey();
+        }
+    }
+}
